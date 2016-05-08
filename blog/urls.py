02161ddd04fe8ec16c1e19360e9d85fc24e1940b
Manuel Perez <manuelph14@gmail.com>
2016-05-07 22:15:02 -0430

    beginning a blog

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.conf.urls import url, include
+from django.views.generic import ListView, DetailView
+from blog.models import Post
+
+# Create your views here.
+urlpatterns = [
+    url(r'^$', ListView.as_view(queryset=Post.objects.all().order_by("-date")[:25], template_name="blog/blog.html"))
+]
